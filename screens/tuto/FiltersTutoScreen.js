5176735006f9100a907de20f12d33bd0059b0260
ilamarre <isabellelamarre62@gmail.com>
2024-12-18 17:47:34 +0100

    tuto1235ok

--- screens/tuto/FiltersTutoScreen.js
+++ screens/tuto/FiltersTutoScreen.js
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native'
 import BottomMenu from './components/BottomMenu'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const FadeInView = ({ delay = 0, duration = 5000, fadeOut = false, children, style }) => {
+const FadeInView = ({ delay = 0, duration = 3000, fadeOut = false, children, style }) => {
     const fadeAnim = useAnimatedValue(fadeOut ? 1 : 0); // Départ selon le sens de l'animation
 
     useEffect(() => {
@@ -34,9 +34,9 @@ export default function FiltersTutoScreen() {
     const [tooltip3Visible, setTooltip3Visible] = useState(false);
 
     useEffect(() => {
-        const timer1 = setTimeout(() => setTooltip1Visible(true), 5000); // Délai pour le premier tooltip   
-        const timer2 = setTimeout(() => setTooltip2Visible(true), 8500); // Délai pour le deuxième tooltip   
-        const timer3 = setTimeout(() => setTooltip3Visible(true), 12000); // Délai pour le troisième tooltip
+        const timer1 = setTimeout(() => setTooltip1Visible(true), 3000); // Délai pour le premier tooltip   
+        const timer2 = setTimeout(() => setTooltip2Visible(true), 4500); // Délai pour le deuxième tooltip   
+        const timer3 = setTimeout(() => setTooltip3Visible(true), 6000); // Délai pour le troisième tooltip
         return () => {
             clearTimeout(timer1);
             clearTimeout(timer2);
@@ -48,11 +48,10 @@ export default function FiltersTutoScreen() {
             <Text style={styles.texttuto}>Tuto 2/5</Text>
             <View style={styles.containermap}>
                 {/* Tooltip avec animation de disparition */}
-                <FadeInView fadeOut={true} duration={7000} style={styles.container}>
+                <FadeInView fadeOut={true} duration={3000} style={styles.container}>
                     <View style={styles.tooltip}>
-                        <Text style={styles.tooltiptext}>Ici tu peux gérer ce que tu souhaites voir sur la carte. Lieux, amis, inconnus etc...</Text>
-
-                        <Icon name="arrow-left" style={[styles.arrow, { transform: [{ rotate: '140deg' }] }]} size={30} color="#000" />
+                        {/* <Text style={styles.tooltiptext}>Ici tu peux choisir le status de ton chien</Text> */}
+                        <Icon name="hand-pointer-o" style={[styles.hand, { transform: [{ rotate: '180deg' }] }]} loop={true} size={50} color="#44CD6F" />
                     </View>
                 </FadeInView>
             </View>
@@ -62,28 +61,28 @@ export default function FiltersTutoScreen() {
             </FadeInView>
 
             {tooltip1Visible && (
-                <FadeInView delay={7500} duration={3000} fadeOut={true} style={styles.menu}>
-                    <View style={styles.tooltipmenu}>
+                <FadeInView delay={500} duration={1000} fadeOut={false} style={styles.menu}>
+                    <View style={styles.tooltip2}>
                         <Text style={styles.tooltiptext}>Ici tu peux choisir le type de carte.</Text>
-                        <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" />
+                        {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
                     </View>
                 </FadeInView>
             )}
 
             {tooltip2Visible && (
-                <FadeInView delay={10500} duration={3000} fadeOut={true} style={styles.menu}>
-                    <View style={styles.tooltipmenu}>
+                <FadeInView delay={500} duration={1000} fadeOut={false} style={styles.menu}>
+                    <View style={styles.tooltip3}>
                         <Text style={styles.tooltiptext}>Ici tu peux choisir les lieux que tu veux afficher sur la carte.</Text>
-                        <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" />
+                        {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
                     </View>
                 </FadeInView>
             )}
 
             {tooltip3Visible && (
-                <FadeInView delay={12000} duration={3000} fadeOut={true} style={styles.menu}>
-                    <View style={styles.tooltipmenu}>
+                <FadeInView delay={500} duration={1000} fadeOut={false} style={styles.menu}>
+                    <View style={styles.tooltip4}>
                         <Text style={styles.tooltiptext}>Ici tu peux choisir les utilisateurs que tu veux afficher sur la carte.</Text>
-                        <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" />
+                        {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
                     </View>
                 </FadeInView>
             )}
@@ -121,7 +120,7 @@ const styles = StyleSheet.create({
         flex: 0.1,
         width: '100%',
         height: '15%',
-        marginBottom: -20,
+        marginBottom: -25,
 
     },
     background: {
@@ -138,37 +137,70 @@ const styles = StyleSheet.create({
     },
     tooltip: {
         position: 'absolute',
-        bottom: -350,
-        //right:-5,// Ajustez cette valeur pour placer le tooltip
+        top: 480,
+        left:-190,
         backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
         padding: 10,
         borderRadius: 8,
         maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
         zIndex: 1,
+        justifyContent: 'center',
+    },
+    tooltip1: {
+        position: 'absolute',
+        //bottom: -400,
+        //right: 20,// Ajustez cette valeur pour placer le tooltip
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 10,
+        borderRadius: 8,
+        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        marginTop: 90,
+        // position: 'absolute',
+        left: - 115,
+        bottom: -25
+    },
+    tooltip2: {
+        position: 'absolute',
+        top: -520,
+        left: 240,// Ajustez cette valeur pour placer le tooltip
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 10,
+        borderRadius: 8,
+        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+
+    },
+    tooltip3: {
+        position: 'absolute',
+        bottom: 290,
+        left: 15,// Ajustez cette valeur pour placer le tooltip
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 10,
+        borderRadius: 8,
+        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 2,
+    },
+    tooltip4: {
+        position: 'absolute',
+        bottom: 130,
+        left: 248,// Ajustez cette valeur pour placer le tooltip
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 10,
+        borderRadius: 8,
+        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 2,
     },
     tooltiptext: {
         color: '#fff',
         fontSize: 14,
         textAlign: 'center',
         lineHeight: 18, // Ajoute un espace suffisant entre les lignes pour une meilleure lisibilité
-
     },
     arrow: {
-        // width: 180,
-        // height: 10,
-        // borderLeftWidth: 2,
-        // borderRightWidth: 2,
-        // borderBottomWidth: 2,
-        // borderStyle: 'solid',
-        //backgroundColor: 'rgba(0, 0, 0,0.8)',
-        // borderLeftColor: 'transparent',
-        // borderRightColor: 'transparent',
-        // borderBottomColor: '#fff',
-        // marginTop: -2, // Ajustez selon l'espace souhaité
-        // position: 'relative',
-        // top: 65,
-        // left: -70,
+        marginTop: -30
     },
+   
     image: {
         width: '100%',
         height: 500,
@@ -178,18 +210,12 @@ const styles = StyleSheet.create({
     imageContainer: {
         marginBottom: -30
     },
-    tooltipmenu: {
-        position: 'absolute',
-        top: -600,
-        right: 140,// Ajustez cette valeur pour placer le tooltip
-        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
-        padding: 10,
-        borderRadius: 8,
-        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
-        zIndex: 3,
-    },
+
     menu: {
-        alignItems: 'top'
+        justifyContent: 'center',
+        alignContent: 'center',
+
+
     },
 
 })
--- screens/tuto/MenuTutoScreen.js
+++ screens/tuto/MenuTutoScreen.js
@@ -1,31 +1,269 @@
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button, ImageBackground, Animated, useAnimatedValue, Image, delay, fadeOut, duration } from 'react-native'
 import { useEffect, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { useNavigation } from '@react-navigation/native'
 import BottomMenu from './components/BottomMenu'
+import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const FadeInView = ({ delay = 0, duration = 5000, fadeOut = false, children, style }) => {
+    const fadeAnim = useAnimatedValue(fadeOut ? 1 : 0); // Départ selon le sens de l'animation
 
-
-export default function MenuTutoScreen(props) {
-    const navigation = useNavigation()
-    const route = useRoute()
+    useEffect(() => {
+        Animated.timing(fadeAnim, {
+            toValue: fadeOut ? 0 : 1, // Aller vers 1 (apparition) ou 0 (disparition)
+            duration: duration, // Durée de l'animation
+            delay: delay, // Délai avant de commencer
+            useNativeDriver: true,
+        }).start();
+    }, [fadeAnim, fadeOut, delay, duration]);
 
     return (
-        <View style={styles.container}>
-            <Text>Route : {route.name}</Text>
-            <BottomMenu navigateTo='Map'/>
-        </View>
+        <Animated.View style={{ ...style, opacity: fadeAnim }}>
+            {children}
+        </Animated.View>
+    );
+};
+
+export default function StatusTutoScreen() {
+    const navigation = useNavigation();
+    const route = useRoute();
+    const [tooltip1Visible, setTooltip1Visible] = useState(false);
+    const [tooltip2Visible, setTooltip2Visible] = useState(false);
+    const [tooltip3Visible, setTooltip3Visible] = useState(false);
+    const [tooltip4Visible, setTooltip4Visible] = useState(false);
+    const [tooltip5Visible, setTooltip5Visible] = useState(false);
+
+
+    useEffect(() => {
+        const timer1 = setTimeout(() => setTooltip1Visible(true), 1500);    
+        const timer2 = setTimeout(() => setTooltip2Visible(true), 3000);    
+        const timer3 = setTimeout(() => setTooltip3Visible(true), 4500); 
+        const timer4 = setTimeout(() => setTooltip4Visible(true), 6000);  
+        const timer5 = setTimeout(() => setTooltip5Visible(true), 7500);
+        return () => {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+            clearTimeout(timer3);
+            clearTimeout(timer4);
+            clearTimeout(timer5);
+        };
+    }, []);
+
+    return (
+        <ImageBackground source={require('../../assets/icons/map_tuto.jpg')} style={styles.background}>
+            <Text style={styles.texttuto}>Tuto 5/5</Text>
+            <View style={styles.containermap}>
+                <FadeInView fadeOut={true} duration={3000} style={styles.container}>
+                    <View style={styles.tooltip}>
+                        {/* <Text style={styles.tooltiptext}>Ici tu peux choisir le status de ton chien</Text> */}
+                        <Icon name="hand-pointer-o" style={[styles.hand, { transform: [{ rotate: '270deg' }] }]} size={50} color="#44CD6F" />
+                    </View>
+                </FadeInView>
+                <FadeInView delay={500} duration={3000} style={styles.imageContainer}>
+                    <Image style={styles.image} source={require('../../assets/icons/menuutilisateur.png')} />
+                </FadeInView>
+                {tooltip1Visible && (
+                    <FadeInView fadeOut={false} duration={3000} style={styles.menu}>
+                        <View style={styles.tooltip1}>
+                            <Text style={styles.tooltiptext}>Ton prénom et ton nom </Text>
+                            {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
+                        </View>
+                    </FadeInView>
+                )}
+                {tooltip2Visible && (
+                    <FadeInView fadeOut={false} duration={3000} style={styles.menu}>
+                        <View style={styles.tooltip2}>
+                            <Text style={styles.tooltiptext}>Ici tu pourras modifier tes informations personelles </Text>
+                            {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
+                        </View>
+                    </FadeInView>
+                )}
+                {tooltip3Visible && (
+                    <FadeInView fadeOut={false} duration={3000} style={styles.menu}>
+                        <View style={styles.tooltip3}>
+                            <Text style={styles.tooltiptext}>Ici tu pourras modifier les informations de ton chien ou ajouter un autre chien </Text>
+                            {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
+                        </View>
+                    </FadeInView>
+                )}
+                {tooltip4Visible && (
+                    <FadeInView fadeOut={false} duration={3000} style={styles.menu}>
+                        <View style={styles.tooltip4}>
+                            <Text style={styles.tooltiptext}>Ici tu pourras accepter ou refuser une invitation ou bloquer un utilisateur.</Text>
+                            {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
+                        </View>
+                    </FadeInView>
+                )}
+                {tooltip5Visible && (
+                    <FadeInView fadeOut={false} duration={3000} style={styles.menu}>
+                        <View style={styles.tooltip5}>
+                            <Text style={styles.tooltiptext}>Ici tu pourras te deconnecter </Text>
+                            {/* <Icon name="arrow-down" style={styles.arrow} size={30} color="#000" /> */}
+                        </View>
+                    </FadeInView>
+                )}
+                <View style={styles.containerbutton}>
+                    <BottomMenu navigateTo='Map' />
+                </View>
+            </View>
+        </ImageBackground>
     )
 }
 
 import { globalStyle } from '../../config'
 // STYLES
+
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: globalStyle.backgroundColor,
+        //backgroundColor: '#000',
+        //opacity: 0.2,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         paddingTop: 20,
+        // height: '90%',
     },
+    containermap: {
+        //backgroundColor: '#000',
+        //opacity: 0.2,
+        flex: 0.9,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 20,
+        height: '100%',
+        marginTop: 150
+    },
+    containerbutton: {
+        flex: 0.1,
+        width: '100%',
+        height: '15%',
+        //marginTop: '50',
+        // marginTop: 50
+        // paddingTop:'20%',
+        //justifyContent:'space between',
+        marginBottom: -95
+
+    },
+    background: {
+        width: '100%',
+        height: '97%',
+    },
+    texttuto: {
+        color: '#000',
+        alignItems: 'center',
+        position: 'relative',
+        top: 20,
+        left: 300,
+        fontSize: globalStyle.h2,
+    },
+    tooltip: {
+        position: 'absolute',
+        top: -170,
+        right: 80,// Ajustez cette valeur pour placer le tooltip
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 10,
+        borderRadius: 8,
+        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        justifyContent: 'center',
+        // marginEnd:10,
+    },
+    tooltip1: {
+        position: 'absolute',
+        top: -770,
+        right: -195,
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 5,
+        borderRadius: 8,
+        maxWidth: 250, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        justifyContent: 'center',
+    },
+    tooltip2: {
+        position: 'absolute',
+        top: -690,
+        right: -195,
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 5,
+        borderRadius: 8,
+        maxWidth: 250, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        justifyContent: 'center',
+    },
+    tooltip3: {
+        position: 'absolute',
+        top: -640,
+        right: -195,
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 5,
+        borderRadius: 8,
+        maxWidth: 246, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        justifyContent: 'center',
+    },
+    tooltip4: {
+        position: 'absolute',
+        top: -570,
+        right: -195,
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 5,
+        borderRadius: 8,
+        maxWidth: 240, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        justifyContent: 'center',
+    },
+    tooltip5: {
+        position: 'absolute',
+        top: -490,
+        right: -195,
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 5,
+        borderRadius: 8,
+        maxWidth: 240, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 1,
+        justifyContent: 'center',
+    },
+    tooltiptext: {
+        color: '#fff',
+        fontSize: 14,
+        textAlign: 'center',
+        lineHeight: 18, // Ajoute un espace suffisant entre les lignes pour une meilleure lisibilité
+
+    },
+    arrow: {
+        position: 'relative',
+        top: 45,
+        left: 50,
+    },
+    image: {
+        height: 800,
+        position: 'absolute',
+        top: -270,
+        right: -91,
+
+        resizeMode: 'contain', // Essayez différents modes
+    },
+    imageContainer: {
+        width: '100%',
+        height: '100%',
+        //  alignItems: 'center',
+        // marginBottom: 5
+
+    },
+    tooltipmenu: {
+        position: 'absolute',
+        top: -100,
+        right: 140,// Ajustez cette valeur pour placer le tooltip
+        backgroundColor: 'rgba(0, 0, 0,1)', // Opacité corrigée pour que le fond soit bien visible
+        padding: 10,
+        borderRadius: 8,
+        maxWidth: 150, // Limite la largeur maximale pour forcer le passage à la ligne
+        zIndex: 3,
+        alignItems: 'center',
+    },
+    menu: {
+        alignItems: 'center'
+    },
+
 })
