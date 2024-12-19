297e61281441441d73391b8f77070e133e2bf218
Fabrice Guichard <fabrice.guichard@gmail.com>
2024-12-19 11:04:24 +0100

    fix auto login

--- screens/login/HomeScreen.js
+++ screens/login/HomeScreen.js
@@ -1,121 +1,127 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useRoute, useNavigation } from '@react-navigation/native'
-import ButtonPrimary from '../../globalComponents/ButtonPrimary'
-import { BACKEND_URL, globalStyle } from '../../config'
-import { logout } from '../../reducers/user'
-import { logoHomeUrl } from '../../config'
-
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import ButtonPrimary from "../../globalComponents/ButtonPrimary";
+import { BACKEND_URL, globalStyle } from "../../config";
+import { logout } from "../../reducers/user";
+import { logoHomeUrl } from "../../config";
 
 export default function HomeScreen(props) {
-    const navigation = useNavigation()
-    const route = useRoute()
-    const user = useSelector(stats => stats.user.value)
-    const dispatch = useDispatch()
-    const [logged, setLogged] = useState(false)
+  const navigation = useNavigation();
+  const route = useRoute();
+  const user = useSelector((stats) => stats.user.value);
+  const dispatch = useDispatch();
+  const [logged, setLogged] = useState(false);
 
-    useEffect(() => {
-        if (route.params?.forceLogout) {
-            handleLogout()
-        }
-    }, [route.params?.forceLogout])
-
-    useEffect(() => {
-        if (user?.token) {
-            fetchUser()
-        }
-        else {
-            setLogged(false)
-        }
-    }, [])
-
-    const handleLogout = async () => {
-        const url = `${BACKEND_URL}/users/${user.token}/status`
-        console.log('PUT', url)
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ status: 'off' })
-        })
-        const data = await response.json()
-        // console.log(data)
-        dispatch(logout())
-        setLogged(false)
-        route.params.forceLogout = false
+  useEffect(() => {
+    if (route.params?.forceLogout) {
+      handleLogout();
     }
+  }, [route.params?.forceLogout]);
 
-    const handleSignUp = () => {
-        navigation.navigate('SignUpUser')
+  useEffect(() => {
+    (async () => {
+      if (user.token) {
+        fetchUser();
+      } else {
+        setLogged(false);
+      }
+    })()
+  }, []);
+
+  const handleLogout = async () => {
+    console.log("handleLogout");
+    const url = `${BACKEND_URL}/users/${user.token}/status`;
+    console.log("PUT", url);
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: "off" }),
+    });
+    const data = await response.json();
+    // console.log(data)
+    dispatch(logout());
+    setLogged(false);
+    route.params.forceLogout = false;
+  };
+
+  const handleSignUp = () => {
+    navigation.navigate("SignUpUser");
+  };
+
+  const handleSignIn = () => {
+    navigation.navigate("SignIn");
+  };
+
+  const fetchUser = async () => {
+    console.log("token exists, auto signin");
+    const url = `${BACKEND_URL}/users/me/${user.token}`;
+    console.log("GET", url);
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.result) {
+      console.log("fetch user ok");
+      setLogged(true);
+      dispatch(setUser(data.data));
     }
+  };
 
-    const handleSignIn = () => {
-        navigation.navigate('SignIn')
-    }
+  console.log(logged);
 
-
-    const fetchUser = async () => {
-        console.log('token exists, auto signin')
-        const url = `${BACKEND_URL}/users/me/${user.token}`
-        console.log('GET', url)
-        const response = await fetch(url)
-        const data = await response.json()
-        if (data.result) {
-            console.log('fetch user ok', data.data)
-            dispatch(setUser(data.data))
-            setLogged(true)
-            return
-        }
-    }
-
-    console.log(logged)
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image source={{ uri: logoHomeUrl }} style={styles.logo}></Image>
-                {/* <Text>Image Logo</Text> */}
-            </View>
-            <View style={styles.buttonsContainer}>
-                {logged && <>
-                    <Text>Bonjour {user.infos.firstname}</Text>
-                    <ButtonPrimary onPress={() => navigation.navigate('Map', { screen: '_Map' })} title='Voir la carte' />
-                </>}
-                {!logged && <>
-                    <ButtonPrimary onPress={() => handleSignUp()} title='Sign Up' />
-                    <ButtonPrimary onPress={() => handleSignIn()} title='Sign In' />
-                </>}
-            </View>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: logoHomeUrl }} style={styles.logo}></Image>
+        {/* <Text>Image Logo</Text> */}
+      </View>
+      <View style={styles.buttonsContainer}>
+        {logged && (
+          <>
+            <Text>Bonjour {user.infos.firstname}</Text>
+            <ButtonPrimary
+              onPress={() => navigation.navigate("Map", { screen: "_Map" })}
+              title="Voir la carte"
+            />
+          </>
+        )}
+        {!logged && (
+          <>
+            <ButtonPrimary onPress={() => handleSignUp()} title="Sign Up" />
+            <ButtonPrimary onPress={() => handleSignIn()} title="Sign In" />
+          </>
+        )}
+      </View>
+    </View>
+  );
 }
 
 // STYLES
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: globalStyle.backgroundColor,
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingTop: 20,
-    },
-    imageContainer: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        height: '80%',
-    },
-    logo: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'contain',
-    },
-    buttonsContainer: {
-        // backgroundColor:'red',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        width: '100%',
-        height: '20%',
-    },
-})
+  container: {
+    backgroundColor: globalStyle.backgroundColor,
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  imageContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "80%",
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  buttonsContainer: {
+    // backgroundColor:'red',
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "100%",
+    height: "20%",
+  },
+});
