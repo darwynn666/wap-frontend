108712573f709701a96bdc4bd63fa95847180350
Julien Garniaux <julien.garniaux@gmail.com>
2024-12-19 12:36:35 +0100

    fix reducer places

---
+++ reducers/places.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: {},
+}
+
+export const placesSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setPlace: (state, action) => {
+            state.value = action.payload
+        },
+    },
+})
+
+export const { setPlace  } = placesSlice.actions
+export default placesSlice.reducer
